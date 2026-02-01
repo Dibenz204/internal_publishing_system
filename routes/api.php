67dd9b3ea5d2e3330ce08f75d6edbf2a4575640c
Nguyen Dinh Phong <dinhphong2222004@gmail.com>
2026-02-01 21:04:44 +0700

    fix bug services, env

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,12 @@
- <?php
+<?php
 
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-    //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    //    return $request->user();
-    // });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-
-    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-
-    // User routes
-    Route::prefix('user')->group(function () {
-        require __DIR__ . '/api/user.php'; // Sẽ handle: /api/user/...
-    });
-
-    //require __DIR__ . '/api/book-category.php';    // URL: /api/book-category/.....
+require __DIR__ . '/api/employeeRoute.php';
+require __DIR__ . '/api/positionRoute.php';
+require __DIR__ . '/api/departmentRoute.php';
