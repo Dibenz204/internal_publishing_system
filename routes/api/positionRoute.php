a6710d1b36cf4444ab30fba0015115226a06e75d
nguyenhuynhlam <lamnguyen.892004@gmail.com>
2026-02-14 17:26:41 +0700

    add position routes

--- routes/api/positionRoute.php
+++ routes/api/positionRoute.php
@@ -4,8 +4,19 @@ use App\Http\Controllers\Api\PositionController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('positions')->group(function () {
+
+    // Lấy danh sách tất hoặc theo tên
     Route::get('/', [PositionController::class, 'index']);
-    Route::get('/{id}', [PositionController::class, 'show']);
+
+    // Tạo mới
     Route::post('/', [PositionController::class, 'store']);
+
+    // Cập nhật
     Route::put('/{id}', [PositionController::class, 'update']);
+
+    // Bật position
+    Route::patch('/{id}/activate', [PositionController::class, 'activate']);
+
+    // Tắt position
+    Route::patch('/{id}/deactivate', [PositionController::class, 'deactivate']);
 });
