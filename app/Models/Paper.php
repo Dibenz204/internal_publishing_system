c3f41ec684f2a1f85b5ee3381c94cb76d6684666
Yen <yen281104@gmail.com>
2026-03-02 13:35:38 +0700

    New table: Paper

---
+++ app/Models/Paper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Paper extends Model
+{
+    use HasFactory;
+        protected $fillable = [
+        'paper_coefficient',
+        'paperSize',
+    ];
+
+    public function book()
+    {
+        return $this->hasMany(Book::class, 'paper_id');
+    }
+
+
+}
