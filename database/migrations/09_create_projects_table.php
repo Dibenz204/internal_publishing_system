8ca2ab5fedf3559e6f9f266f18699e04fa624af8
Nguyen Dinh Phong <dinhphong2222004@gmail.com>
2026-03-04 19:30:12 +0700

    merge everything into Frontend, fixing route for use with correctly position

---
+++ database/migrations/09_create_projects_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id(); //NOT NULL
+
+            $table->string('description')->nullable();
+ 
+            $table->unsignedTinyInteger('status') //NOT NULL
+            ->default(1)
+            ->comment('0=cancel,1=accepted,2=processing,3=completed'); //có 4 trạng thái, sử dụng kiểu unsignedTinyInteger cho đỡ tốn bộ nhớ
+
+            $table->foreignId('department_id') 
+            ->nullable()
+            ->constrained('departments')
+            ->nullOnDelete();
+
+            $table->foreignId('book_id') //NOT NULL
+            ->constrained('books')
+            ->cascadeOnDelete();
+
+            $table->unique(['department_id', 'book_id']);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
