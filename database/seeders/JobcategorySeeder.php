3e8798926e4fb8ed70b737360b827a11ac1140a9
Nguyen Dinh Phong <dinhphong2222004@gmail.com>
2026-03-15 15:45:26 +0700

    adjust seeder data

--- database/seeders/JobcategorySeeder.php
+++ database/seeders/JobcategorySeeder.php
@@ -11,23 +11,15 @@ class JobCategorySeeder extends Seeder
     {
         $categories = [
 
-            ['name' => 'Biên tập', 'category' => 'A', 'work_coefficient' => 0.25, 'status' => 1],
-            ['name' => 'Biên tập', 'category' => 'B', 'work_coefficient' => 1.00, 'status' => 1],
-            ['name' => 'Biên tập', 'category' => 'C', 'work_coefficient' => 1.20, 'status' => 1],
-            ['name' => 'Biên tập', 'category' => 'D', 'work_coefficient' => 2.00, 'status' => 1],
-            ['name' => 'Biên tập', 'category' => 'E', 'work_coefficient' => 0.40, 'status' => 1],
-            ['name' => 'Biên tập', 'category' => 'F', 'work_coefficient' => 0.60, 'status' => 1],
-            ['name' => 'Biên tập', 'category' => 'G', 'work_coefficient' => 1.10, 'status' => 1],
+            ['name' => 'Biên tập 100%', 'category' => 'Biên tập', 'work_coefficient' => 0.25, 'status' => 1],
+            ['name' => 'Biên tập 70%', 'category' => 'Biên tập', 'work_coefficient' => 1.00, 'status' => 1],
+            ['name' => 'Biên tập 50%', 'category' => 'Biên tập', 'work_coefficient' => 1.20, 'status' => 1],
+            ['name' => 'Biên tập 40%', 'category' => 'Biên tập', 'work_coefficient' => 2.00, 'status' => 1],
 
 
-            ['name' => 'Đọc đính chính', 'category' => 'A', 'work_coefficient' => 0.02, 'status' => 1],
-            ['name' => 'Đọc đính chính', 'category' => 'B', 'work_coefficient' => 0.015, 'status' => 1],
+            ['name' => 'Đọc logic', 'category' => 'Đọc đính chính', 'work_coefficient' => 0.02, 'status' => 1],
 
-                        
-            ['name' => 'Sửa bài', 'category' => 'A', 'work_coefficient' => 0.03, 'status' => 1],
-            ['name' => 'Sửa bài', 'category' => 'B', 'work_coefficient' => 0.025, 'status' => 1],
-
-
+            ['name' => 'Sửa bài', 'category' => 'Sửa bài', 'work_coefficient' => 0.03, 'status' => 1],
         ];
 
         foreach ($categories as $item) {
